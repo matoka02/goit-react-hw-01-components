1f966608c47721b12b0cd913c75eab961579a41a
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-05 19:33:51 +0300

    delete

--- src/index.js
+++ src/index.js
@@ -12,11 +12,6 @@ import './index.css';
 import {Profile} from './components/01-network/Profile';
 import user from './components/01-network/user.json';
 
-import {Statistics} from './components/02-statistics/Statistics';
-import data from './components/02-statistics/data.json';
-
-import { FriendList } from 'components/03-friends-list/FriendList';
-import friends from './components/03-friends-list/friends.json';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -29,10 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics 
-        title='Upload stats' 
-        stats={data} /> 
-      <FriendList friends={friends}/>     
+   
     </App>
   </React.StrictMode>
 );
