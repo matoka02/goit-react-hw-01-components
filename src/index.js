8d0bab66acc30fe00754f3bbdaf4fcfb1aba0745
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-05 19:16:25 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,25 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import {Profile} from './components/01-network/Profile';
+import user from './components/01-network/user.json';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </App>
   </React.StrictMode>
 );
