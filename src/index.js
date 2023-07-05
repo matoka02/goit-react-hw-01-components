73e53b06eaeb0bb1eefba68b1abacf8b887d4e3a
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-05 19:21:02 +0300

    create task-02

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ import './index.css';
 import {Profile} from './components/01-network/Profile';
 import user from './components/01-network/user.json';
 
+import {Statistics} from './components/02-statistics/Statistics';
+import data from './components/02-statistics/data.json';
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App>
@@ -22,6 +26,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         avatar={user.avatar}
         stats={user.stats}
       />
+        <Statistics 
+        title='Upload stats' 
+        stats={data} />      
     </App>
   </React.StrictMode>
 );
