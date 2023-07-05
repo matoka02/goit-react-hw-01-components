a7b019bcc560dbdd4a49f2270412fa1733c29ab1
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-05 19:47:43 +0300

    update task-03

---
+++ src/components/03-friends-list/FriendList.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from './FriendListItem';
+import friends from './friends.json';
+import css from './Friends.module.css';
+
+// console.log(data);
+
+export const FriendList = ({ friends }) => (
+    <ul className={css.friendList}>
+        {friends.map(friend => (
+        <FriendListItem
+            key={friend.id}
+            avatar={friend.avatar}
+            isOnline={friend.isOnline}
+            name={friend.name}
+        ></FriendListItem>
+        ))}
+    </ul>
+);
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(
+        PropTypes.shape({
+            id: PropTypes.number.isRequired,
+            avatar: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            isOnline: PropTypes.bool.isRequired,
+        })
+    ).isRequired,
+};
