a7e571a83211620ef0ddcbda02306caaf48542fa
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-05 19:38:05 +0300

    delete

--- src/index.js
+++ src/index.js
@@ -12,9 +12,6 @@ import './index.css';
 import {Profile} from './components/01-network/Profile';
 import user from './components/01-network/user.json';
 
-import {Statistics} from './components/02-statistics/Statistics';
-import data from './components/02-statistics/data.json';
-
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -27,9 +24,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         avatar={user.avatar}
         stats={user.stats}
       />
-      <Statistics 
-        title='Upload stats' 
-        stats={data} /> 
+
  
     </App>
   </React.StrictMode>
