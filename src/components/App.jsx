30f57ad41db827d7f04920976c5f0efc6b111797
matoka02 <a.m.nigomedzyanova@gmail.com>
2023-07-07 18:41:44 +0300

    update after fixing problems

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,18 @@
+import React from 'react';
+
+
+import { Profile } from "./01-network/Profile";
+import user from '../components/01-network/user.json';
+
+import { Statistics } from "./02-statistics/Statistics";
+import data from '../components/02-statistics/data.json';
+
+import { FriendList } from "./03-friends-list/FriendList";
+import friends from '../components/03-friends-list/friends.json';
+
+import { TransactionHistory } from "./04-transaction-history/TransactionHistory";
+import transactions from '../components/04-transaction-history/transactions.json';
+
 export const App = ({children}) => {
   return (
     <div
@@ -11,7 +26,21 @@ export const App = ({children}) => {
         color: '#010101'
       }}
     >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics 
+        title='Upload stats' 
+        stats={data} />
+      <FriendList friends={friends}/> 
+      <TransactionHistory items={transactions} />   
       {children}
     </div>
   );
 };
+
+export default App
--- src/index.js
+++ src/index.js
@@ -3,41 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
-import {Profile} from './components/01-network/Profile';
-import user from './components/01-network/user.json';
-
-import {Statistics} from './components/02-statistics/Statistics';
-import data from './components/02-statistics/data.json';
-
-import { FriendList } from 'components/03-friends-list/FriendList';
-import friends from './components/03-friends-list/friends.json';
-
-import { TransactionHistory } from 'components/04-transaction-history/TransactionHistory';
-import transactions from './components/04-transaction-history/transactions.json';
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App>
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics 
-        title='Upload stats' 
-        stats={data} />
-      <FriendList friends={friends}/> 
-      <TransactionHistory items={transactions} />      
-    </App>
+    <App />
   </React.StrictMode>
 );
 
